2ce8daad4007c930b22b17d12be171a7219febb4
Ming <ming@gnim.net>
2014-09-30 09:54:21 +0800

    增加表格末尾行固定行参数支持

--- tablesort-min.js
+++ tablesort-min.js
@@ -1,9 +1,9 @@
 /*
  *  Table Sort support
  *  Author:Ming
- *  Version:1.1.0
- *  Date:2012.02.12
+ *  Version:1.2.0
+ *  Date:2014.09.30
  */
-(function(d){function h(a,i){var f=d(a);if(1!=f.length)f.each(function(c){new h(c,i)});else{for(var e=f.find("th").addClass(p),c=0;c<e.length;c++)(function(c,a){d(c).click(function(){var g=d(c).find("."+j),e=0,b=j;0<g.length&&!g.hasClass(k)&&(e=1,b+=" "+k);f.find("."+j).remove();d(c).append('<span class="'+b+'"></span>');q(f,a,e,i)})})(e[c],c);this.$dom=f}}function q(a,i,f,e){var c=[],j=0,h=1,g=a.find("tbody tr").each(function(a){a=d(a).find("td");if(i<a.length){var b=a[i],a=d(b).text();""==d.trim(a)&&
-(b=d(b).find("input"),0<b.length&&(b=b[0],"checked"in b?a=b.checked?1:0:"value"in b&&(a=b.value)));null!=a&&""!=a&&!isNaN(a)||(h=0);c.push({i:j++,t:a})}}),l=1;for(;l<c.length;l++)for(var b=c.length-1;b>=l;b--){var m=c[b],n=c[b-1],o=h?parseFloat(m.t)<parseFloat(n.t):m.t<n.t;f&&(o=!o);o&&(c[b-1]=m,c[b]=n)}for(var k in c)d(g[c[k].i]).appendTo(a.find("tbody"));if(e){g=a.find("tbody tr").removeClass(e);for(a=0;a<g.length;a++)1==a%2&&d(g[a]).addClass(e)}}var p="table-sort-th",j="table-sort-arrow",k="table-sort-arrow-desc";
-window.TableSort=h})(Gnim);
+(function(d,t,w){function l(a,m,h){var b=d(a);if(1!==b.length)b.each(function(c){new l(c,m,h)});else{a=b.find("th").addClass(u);for(var e=0;e<a.length;e++)(function(c,a){d(c).click(function(){var e=d(c).find("."+n),f=0,k=n;0<e.length&&!e.hasClass(p)&&(f=1,k+=" "+p);b.find("."+n).remove();d(c).append('<span class="'+k+'"></span>');v(b,a,f,m,h)})})(a[e],e);this.$dom=b}}function v(a,m,h,b,e){var c=[],n=0,l=1,f=a.find("tbody tr").each(function(a){a=d(a).find("td");if(m<a.length){var b=a[m];a=d(b).text();
+""===d.trim(a)&&(b=d(b).find("input"),0<b.length&&(b=b[0],"checked"in b?a=b.checked?1:0:"value"in b&&(a=b.value)));if(a===t||""===a||isNaN(a))l=0;c.push({i:n++,t:a})}}),k=1;for(;k<c.length;k++)for(var g=c.length-1;g>=k;g--){var q=c[g],r=c[g-1],s=l?parseFloat(q.t)<parseFloat(r.t):q.t<r.t;h&&(s=!s);s&&(c[g-1]=q,c[g]=r)}h=a.find("tbody");for(var p in c)d(f[c[p].i]).appendTo(h);if(e)for(g=f.length-e;g<f.length;g++)d(f[g]).appendTo(h);if(b)for(f=a.find("tbody tr").removeClass(b),a=0;a<f.length;a++)1===
+a%2&&d(f[a]).addClass(b)}var u="table-sort-th",n="table-sort-arrow",p="table-sort-arrow-desc";window.TableSort=l})(Gnim,null);
--- tablesort.js
+++ tablesort.js
@@ -1,48 +1,49 @@
 /*
  *  Table Sort support
  *  Author:Ming
- *  Version:1.1.0
- *  Date:2012.02.12
+ *  Version:1.2.0
+ *  Date:2014.09.30
  */
-(function($){
-    var CLASS_TH='table-sort-th';
-    var CLASS_ARROW='table-sort-arrow';
-    var CLASS_ARROW_DESC='table-sort-arrow-desc';
+(function($, NULL, UNDEFINED) {
+    var CLASS_TH = 'table-sort-th';
+    var CLASS_ARROW = 'table-sort-arrow';
+    var CLASS_ARROW_DESC = 'table-sort-arrow-desc';
     /**
      * constructor of table sort
      * @param selector container selector
      * @param highlightClass row hightlight class name
+     * @param footerCnt column numbers keep unsort
      */
-    function TableSort(selector,highlightClass){
-        var objThis=this;
-        var $table=$(selector);
-        if($table.length!=1){
-            $table.each(function(table){
-                new TableSort(table,highlightClass);
+    function TableSort(selector, highlightClass, footerCnt) {
+        var objThis = this;
+        var $table = $(selector);
+        if ($table.length !== 1) {
+            $table.each(function(table) {
+                new TableSort(table, highlightClass, footerCnt);
             });
             return;
         }
-        var $th=$table.find('th').addClass(CLASS_TH);
-        for(var i=0;i<$th.length;i++){
-            (function(th,index){
-                $(th).click(function(){
-                    var $arrow=$(th).find('.'+CLASS_ARROW);
-                    var desc=0;
-                    var cName=CLASS_ARROW;
-                    if($arrow.length>0 && !$arrow.hasClass(CLASS_ARROW_DESC)){
-                        desc=1;
-                        cName+=' '+CLASS_ARROW_DESC;
+        var $th = $table.find('th').addClass(CLASS_TH);
+        for (var i = 0; i < $th.length; i++) {
+            (function(th, index) {
+                $(th).click(function() {
+                    var $arrow = $(th).find('.' + CLASS_ARROW);
+                    var desc = 0;
+                    var cName = CLASS_ARROW;
+                    if ($arrow.length > 0 && !$arrow.hasClass(CLASS_ARROW_DESC)) {
+                        desc = 1;
+                        cName += ' ' + CLASS_ARROW_DESC;
                     }
-                    $table.find('.'+CLASS_ARROW).remove();//remove old arrow
-                    $(th).append('<span class="'+cName+'"></span>');//append new arrow
-                    _sort($table,index,desc,highlightClass);
+                    $table.find('.' + CLASS_ARROW).remove();//remove old arrow
+                    $(th).append('<span class="' + cName + '"></span>');//append new arrow
+                    _sort($table, index, desc, highlightClass, footerCnt);
                 });
-            })($th[i],i);
+            })($th[i], i);
         }
-        objThis.$dom=$table;
+        objThis.$dom = $table;
     }
-    function _isNum(text){
-        return text!=null && text!='' && !isNaN(text);
+    function _isNum(text) {
+        return text !== NULL && text !== '' && !isNaN(text);
     }
     /**
      * sort table
@@ -50,63 +51,71 @@
      * @param index sort column index
      * @param desc descending
      * @param highlightClass row hightlight class name
+     * @param footerCnt column numbers keep unsort
      */
-    function _sort($table,index,desc,highlightClass){
-        var rows=[];
-        var cnt=0;
-        var isNum=1;
-        var $tr=$table.find('tbody tr').each(function(tr){
-            var allTd=$(tr).find('td');
-            if(index<allTd.length){
-                var td=allTd[index];
-                var text=$(td).text();
-                if($.trim(text)==''){
-                    var $input=$(td).find('input');
-                    if($input.length>0){
-                        var input=$input[0];
-                        if('checked' in input){
-                            text=input.checked?1:0;
-                        }else if('value' in input){
-                            text=input.value;
+    function _sort($table, index, desc, highlightClass, footerCnt) {
+        var rows = [];
+        var cnt = 0;
+        var isNum = 1;
+        var $tr = $table.find('tbody tr').each(function(tr) {
+            var allTd = $(tr).find('td');
+            if (index < allTd.length) {
+                var td = allTd[index];
+                var text = $(td).text();
+                if ($.trim(text) === '') {
+                    var $input = $(td).find('input');
+                    if ($input.length > 0) {
+                        var input = $input[0];
+                        if ('checked' in input) {
+                            text = input.checked ? 1 : 0;
+                        } else if ('value' in input) {
+                            text = input.value;
                         }
                     }
                 }
-                if(!_isNum(text))isNum=0;
+                if (!_isNum(text))
+                    isNum = 0;
                 rows.push({
-                    i:cnt++,
-                    t:text
+                    i: cnt++,
+                    t: text
                 });
             }
         });
         //do sort
-        for(var i=1;i<rows.length;i++){
-            for(var j=rows.length-1;j>=i;j--){
-                var row1=rows[j];
-                var row2=rows[j-1];
-                var swap=isNum?(parseFloat(row1.t)<parseFloat(row2.t)):(row1.t<row2.t);
-                if(desc){
-                    swap=!swap;
+        for (var i = 1; i < rows.length; i++) {
+            for (var j = rows.length - 1; j >= i; j--) {
+                var row1 = rows[j];
+                var row2 = rows[j - 1];
+                var swap = isNum ? (parseFloat(row1.t) < parseFloat(row2.t)) : (row1.t < row2.t);
+                if (desc) {
+                    swap = !swap;
                 }
-                if(swap){
-                    rows[j-1]=row1;
-                    rows[j]=row2;
+                if (swap) {
+                    rows[j - 1] = row1;
+                    rows[j] = row2;
                 }
             }
         }
         //reset table element
-        for(var r in rows){
-            $($tr[rows[r].i]).appendTo($table.find('tbody'));
+        var $tbody = $table.find('tbody');
+        for (var r in rows) {
+            $($tr[rows[r].i]).appendTo($tbody);
         }
-        if(highlightClass){
-            $tr=$table.find('tbody tr').removeClass(highlightClass);
-            for(var k=0;k<$tr.length;k++){
-                if(k%2==1){
+        if (footerCnt) {
+            for (var j = $tr.length - footerCnt; j < $tr.length; j++) {
+                $($tr[j]).appendTo($tbody);
+            }
+        }
+        if (highlightClass) {
+            $tr = $table.find('tbody tr').removeClass(highlightClass);
+            for (var k = 0; k < $tr.length; k++) {
+                if (k % 2 === 1) {
                     $($tr[k]).addClass(highlightClass);
                 }
             }
         }
     }
     //retisger to window
-    window.TableSort=TableSort;
-})(Gnim);
+    window.TableSort = TableSort;
+})(Gnim, null);
 
